9aa2ecb9b4fd6e853784a59b9627fe27059ec831
Arthur Richard <arthur.richard@protonmail.com>
2023-08-04 10:44:22 +0200

    :bookmark: 1.1.0 -> 1.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="immosheets",
-    version="1.1.0",
+    version="1.1.1",
     author="Arthur RICHARD",
     author_email="arthur.richard@protonmail.com",
     description="Tired of searching with your mouse ? Let's automate the process.",
