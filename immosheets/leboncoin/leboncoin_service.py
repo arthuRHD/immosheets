cf668a57b2fc275a4670bdc142ca9c177fe2b04e
Arthur Richard <arthur.richard@protonmail.com>
2023-08-10 10:24:32 +0000

    :wastebasket: : Added deprecation warning for LeboncoinService

--- immosheets/leboncoin/leboncoin_service.py
+++ immosheets/leboncoin/leboncoin_service.py
@@ -1,4 +1,5 @@
 import requests
+from deprecated import deprecated
 from ..target import Target
 from ..real_estate import RealEstate
 from .leboncoin_search_query import LeboncoinSearchQuery
@@ -7,15 +8,15 @@ from ..settings import settings
 
 
 class LeboncoinService(RealEstateService):
+    @deprecated(version='1.1.1', reason="leboncoin is now using datadome which bans bots.")
     def __init__(self, api_key: str) -> None:
         self.url = settings.leboncoin_url
         self.headers = {
             "X-RapidAPI-Key": api_key,
             "X-RapidAPI-Host": settings.leboncoin_host
         }
-        raise DeprecationWarning(
-            message="leboncoin is now using datadome which bans bots.")
 
+    @deprecated(version='1.1.1', reason="leboncoin is now using datadome which bans bots.")
     def search(self, query: LeboncoinSearchQuery) -> list[RealEstate]:
         response = requests.post(
             self.url,
